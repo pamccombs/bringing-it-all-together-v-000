8ae81e16a2532f26838ee76a5cbed5de5d53a781
Patrick McCombs <pa.mccombs1@gmail.com>
2019-03-04 09:18:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -75,6 +75,13 @@ class Dog
     end
     dog
   end
+  
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    self.new(id: id, name: name, breed: breed)
+  end
 
   
   
